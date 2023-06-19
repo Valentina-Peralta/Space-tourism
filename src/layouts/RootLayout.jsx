35ed9e2a3f1806271561e3beb1c1d8fe4fab51c5
Valentina-Peralta <125395224+Valentina-Peralta@users.noreply.github.com>
2023-06-18 23:43:40 -0300

    mobile image background instead of the video one

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Nav from '../components/Nav'
 import backgroundVideo from '../assets/space.mp4'
-
+import SpaceImg from '../assets/space-img.jpg'
 
 function RootLayout() {
     return (
         <div >
             <video
+                className='video-background'
                 autoPlay
                 loop
                 muted
@@ -24,6 +25,20 @@ function RootLayout() {
                 <source src={backgroundVideo} type="video/mp4" />
                 Tu navegador no soporta el elemento de video.
             </video>
+            <img
+                className='img-background'
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    width: '100vw',
+                    // maxWidth: '1400px',
+                    height: '100%',
+                    objectFit: 'cover',
+                    objectPosition: 'top left'
+                }}
+                src={SpaceImg} />
+
             <Nav />
             <Outlet />
         </div>
